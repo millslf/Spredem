d5d987ab8443d75f32667b9ba787d325c6642049
francoism <francoism@spsi.co.za>
2018-01-17 10:04:39 +0200

    Refactor exchange model

--- src/main/java/za/co/jefdev/persistence/CoinapultEntity.java
+++ src/main/java/za/co/jefdev/persistence/CoinapultEntity.java
@@ -18,6 +18,18 @@ public class CoinapultEntity extends BaseExchangeEntity {
         jsonObject = new JSONObject(Rest.makeRequest("https://api.coinapult.com/api/ticker?market=GBP_BTC&filter=small"));
         allPairsDelimited = allPairsDelimited + "BTCGBP" +
                 jsonObject.getJSONObject("small").getDouble("ask") + ",";
+        jsonObject = new JSONObject(Rest.makeRequest("https://api.coinapult.com/api/ticker?market=USD_DASH&filter=small"));
+        allPairsDelimited = allPairsDelimited + "DASHUSD" +
+                jsonObject.getJSONObject("small").getDouble("ask") + ",";
+        jsonObject = new JSONObject(Rest.makeRequest("https://api.coinapult.com/api/ticker?market=EUR_DASH&filter=small"));
+        allPairsDelimited = allPairsDelimited + "DASHEUR" +
+                jsonObject.getJSONObject("small").getDouble("ask") + ",";
+        jsonObject = new JSONObject(Rest.makeRequest("https://api.coinapult.com/api/ticker?market=GBP_DASH&filter=small"));
+        allPairsDelimited = allPairsDelimited + "DASHGBP" +
+                jsonObject.getJSONObject("small").getDouble("ask") + ",";
+        jsonObject = new JSONObject(Rest.makeRequest("https://api.coinapult.com/api/ticker?market=BTC_DASH&filter=small"));
+        allPairsDelimited = allPairsDelimited + "DASHBTC" +
+                jsonObject.getJSONObject("small").getDouble("ask") + ",";
         allPairsDelimited = allPairsDelimited.substring(0, allPairsDelimited.length() - 1);
         FileReaderWriter.persistEntities(this);
     }
